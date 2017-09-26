52deb3fc5350ba5efd6d9650bf94b7200c23cbcf
Tristan971 <tristandeloche@gmail.com>
2017-09-26 17:27:11 +0200

    Basic Spring test for EasyFxml autowiring.

--- src/test/java/moe/tristan/easyfxml/model/fxml/BaseEasyFxmlTest.java
+++ src/test/java/moe/tristan/easyfxml/model/fxml/BaseEasyFxmlTest.java
@@ -1,13 +1,26 @@
 package moe.tristan.easyfxml.model.fxml;
 
+import moe.tristan.easyfxml.spring.SpringContext;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration(classes = SpringContext.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class BaseEasyFxmlTest {
+
+    @Autowired
+    private BaseEasyFxml baseEasyFxml;
+
+    @Before
+    public void setUp() {
+        assertThat(this.baseEasyFxml).isNotNull();
+    }
 
     @Test
     public void getPaneForView() {
