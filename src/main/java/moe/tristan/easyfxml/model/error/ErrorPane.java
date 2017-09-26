f284489345fce106937d63e8d706f9a607965f40
Tristan971 <tristandeloche@gmail.com>
2017-09-26 15:16:42 +0200

    Add utility classes and rename some other classes

---
+++ src/main/java/moe/tristan/easyfxml/model/error/ErrorPane.java
@@ -0,0 +1,44 @@
+package moe.tristan.easyfxml.model.error;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import lombok.extern.slf4j.Slf4j;
+import moe.tristan.easyfxml.lang.javafx.DomUtils;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Slf4j
+public final class ErrorPane {
+    private static final double ERROR_FIELD_MARGIN_SIZE = 20.0;
+    private final Throwable exception;
+
+    public ErrorPane(final Throwable exception) {
+        log.debug("Generating ErrorPane for unexpected exception of type {}", exception.getClass());
+        this.exception = exception;
+    }
+
+    public Pane asPane() {
+        return this.asPane(this.exception.getMessage());
+    }
+
+    public Pane asPane(final String userReadableError) {
+        log.debug("Generating node corresponding to ErrorPane...");
+        final Label messageLabel = new Label(userReadableError);
+        final TextArea throwableDataLabel = new TextArea(formatErrorMessage(this.exception));
+
+        DomUtils.centerNode(throwableDataLabel, ERROR_FIELD_MARGIN_SIZE);
+        return new AnchorPane(messageLabel, throwableDataLabel);
+    }
+
+    private static String formatErrorMessage(final Throwable throwable) {
+        return "Message : \n" +
+                throwable.getMessage() +
+                "\nStackTrace:\n" +
+                Arrays.stream(throwable.getStackTrace())
+                        .map(StackTraceElement::toString)
+                        .collect(Collectors.joining("\n"));
+    }
+}
--- src/main/java/moe/tristan/easyfxml/model/views/ViewsManager.java
+++ src/main/java/moe/tristan/easyfxml/model/views/ViewsManager.java
@@ -1,8 +1,11 @@
 package moe.tristan.easyfxml.model.views;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import lombok.extern.slf4j.Slf4j;
 import moe.tristan.easyfxml.EasyFxml;
 import moe.tristan.easyfxml.FxmlFile;
+import moe.tristan.easyfxml.model.error.ErrorPane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
@@ -14,10 +17,15 @@ import org.springframework.stereotype.Service;
  * directly opening a window which's base FXML file has a {@link FxmlFile}
  * instance counterpart.
  *
+ * It provides :
+ * - Error handling ({@link #loadingError(Throwable)}
+ * - Pop-up error handling
+ *
  * It is recommended to use it but {@link EasyFxml} works fine without it.
  */
 @Lazy
 @Service
+@Slf4j
 public class ViewsManager {
 
     private final ApplicationContext context;
@@ -27,12 +35,28 @@ public class ViewsManager {
         this.context = context;
     }
 
-    public Pane loadPaneForView(final FxmlFile fxmlFile) {
+    public Pane loadPaneForView(final FxmlFile fxmlFile, final EOnExceptionBehavior onExceptionBehavior) {
+        log.debug("Loading view : {} [{}]", fxmlFile, fxmlFile.getPath());
         final EasyFxml easyFxml = this.context.getBean(EasyFxml.class);
-        return easyFxml.getPaneForView(fxmlFile).getOrElseGet(this::loadingError);
+        return easyFxml.getPaneForView(fxmlFile).getOrElseGet(exception -> {
+            switch (onExceptionBehavior) {
+                case INLINE:
+                    return this.loadingError(exception);
+                case DIALOG:
+                    this.loadingErrorDialog(exception);
+                    return new Pane(new Label("An error occured. See dialog and/or logs."));
+                default:
+                    throw new RuntimeException("Error behavior was not recognized. Was \""+ onExceptionBehavior +"\".");
+            }
+        });
     }
 
     private Pane loadingError(final Throwable exception) {
-        return null;
+        log.error("Got error loading view ! See stacktrace : ", exception);
+        return new ErrorPane(exception).asPane("Could not load component :( Details :");
+    }
+
+    private void loadingErrorDialog(final Throwable exception) {
+        final Pane errPane = this.loadingError(exception);
     }
 }
