e4198e652c3a430a59ddd9708f397d71cceca996
Tristan971 <tristandeloche@gmail.com>
2017-09-28 12:27:16 +0200

    Lombok set-up for now. We will see how things evolve and if it's manageable to keep it.

---
+++ src/main/java/moe/tristan/easyfxml/spring/__main_stub.java
@@ -0,0 +1,16 @@
+package moe.tristan.easyfxml.spring;
+
+/**
+ * Stub class for the maven-shade-plugin which packages the library as a nice and reusable jar file.
+ * It requires a main class so we provide it with this. Feels like the C days huh.
+ */
+public final class __main_stub {
+
+    private __main_stub() {
+        throw new Error("Should not create such an instance.");
+    }
+
+    public static void main(final String[] args) {
+        throw new UnsupportedOperationException("This is a stub so the jar can be made as it needs a main class");
+    }
+}
