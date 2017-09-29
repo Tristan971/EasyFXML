8495dc1efd3b2dff59f8a89d32946c81ad6a8329
Tristan971 <tristandeloche@gmail.com>
2017-09-29 11:54:12 +0200

    Add some debug logging to StageUtils.

--- src/main/java/moe/tristan/easyfxml/util/StageUtils.java
+++ src/main/java/moe/tristan/easyfxml/util/StageUtils.java
@@ -5,7 +5,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public final class StageUtils {
 
     private StageUtils() {}
@@ -18,6 +20,11 @@ public final class StageUtils {
     }
 
     public static void scheduleDisplaying(final Stage stage) {
+        log.debug(
+                "Requested displaying of stage {} with title : \"{}\"",
+                stage,
+                stage.getTitle()
+        );
         Platform.runLater(stage::show);
     }
 }
