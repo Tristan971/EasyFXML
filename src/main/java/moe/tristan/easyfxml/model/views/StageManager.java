7a1d552f076bea4bd6df94cfa8850b587f984e82
Tristan971 <tristandeloche@gmail.com>
2017-09-29 14:50:05 +0200

    Split up the ControllerManager in one Stage and one Controller specs.

--- src/main/java/moe/tristan/easyfxml/model/views/StageManager.java
+++ src/main/java/moe/tristan/easyfxml/model/views/StageManager.java
@@ -1,7 +1,8 @@
 package moe.tristan.easyfxml.model.views;
 
+import javafx.stage.Stage;
 import moe.tristan.easyfxml.FxmlController;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ControllerManager extends AbstractManager<FxmlController, FxmlController> {}
+public class StageManager extends AbstractManager<FxmlController, Stage> {}
