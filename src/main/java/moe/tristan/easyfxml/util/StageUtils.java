65370b3e570949719964624014592556f68967a6
Tristan971 <tristandeloche@gmail.com>
2017-09-29 14:49:20 +0200

    Convenience auto-displaying method using StageUtils for that.

--- src/main/java/moe/tristan/easyfxml/util/StageUtils.java
+++ src/main/java/moe/tristan/easyfxml/util/StageUtils.java
@@ -7,6 +7,10 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
+
 @Slf4j
 public final class StageUtils {
 
@@ -19,12 +23,30 @@ public final class StageUtils {
         return stage;
     }
 
-    public static void scheduleDisplaying(final Stage stage) {
+    public static Future<Stage> scheduleDisplaying(final Stage stage) {
         log.debug(
                 "Requested displaying of stage {} with title : \"{}\"",
                 stage,
                 stage.getTitle()
         );
-        Platform.runLater(stage::show);
+        return asyncStageOperation(stage, Stage::show);
+    }
+
+    public static Future<Stage> scheduleHiding(final Stage stage) {
+        log.debug(
+                "Requested hiding of stage {} with title : \"{}\"",
+                stage,
+                stage.getTitle()
+        );
+        return asyncStageOperation(stage, Stage::hide);
+    }
+
+    private static Future<Stage> asyncStageOperation(final Stage stage, final Consumer<Stage> asyncOp) {
+        final CompletableFuture<Stage> onAsyncOpDone = new CompletableFuture<>();
+        Platform.runLater(() -> {
+            asyncOp.accept(stage);
+            onAsyncOpDone.complete(stage);
+        });
+        return onAsyncOpDone;
     }
 }
