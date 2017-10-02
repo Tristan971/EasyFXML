a444717f5716bb5333713a442bef14de1d892da2
Tristan971 <tristandeloche@gmail.com>
2017-10-02 16:00:04 +0200

    Thinking about the final usage method

---
+++ src/main/java/moe/tristan/easyfxml/FxmlNode.java
@@ -0,0 +1,15 @@
+package moe.tristan.easyfxml;
+
+/**
+ * A {@link FxmlNode} is any element that the user is meant to see/interract with.
+ *
+ * It is described by 3 elements :
+ * - {@link FxmlFile} : The file which describes its DOM
+ * - {@link FxmlController} class : The class from which we shoudl create its controller (its logic)
+ * - {@link FxmlStylesheet} : The stylesheet aplied to it. Or {@link FxmlStylesheet#NONE} if none in particular.
+ */
+public interface FxmlNode {
+    FxmlFile getFxmlFile();
+    Class<? extends FxmlController> getControllerClass();
+    FxmlStylesheet getStylesheet();
+}
