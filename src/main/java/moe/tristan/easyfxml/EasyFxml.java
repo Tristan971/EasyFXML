4c3f8c59e475c0b9994fab0892530fd50b793e39
Tristan Deloche <tristandeloche@gmail.com>
2017-10-04 11:15:20 +0200

    Move FXMLLoader conf to EasyFxml implementations and adapt ControllerManager usage for it.

--- src/main/java/moe/tristan/easyfxml/EasyFxml.java
+++ src/main/java/moe/tristan/easyfxml/EasyFxml.java
@@ -1,8 +1,10 @@
 package moe.tristan.easyfxml;
 
 import io.vavr.control.Try;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
+import moe.tristan.easyfxml.model.beanmanagement.ControllerManager;
 import moe.tristan.easyfxml.model.fxml.BaseEasyFxml;
 
 public interface EasyFxml {
@@ -10,19 +12,28 @@ public interface EasyFxml {
     /**
      * Loads a {@link FxmlNode} as a Pane (this is the safest base class for all sorts of hierarchies)
      * since most of the base containers are subclasses of it.
+     * It also registers its controller in the {@link ControllerManager} for later usage.
      *
      * It returns a {@link Try} which is a monadic structure which allows us to do clean exception-handling.
      *
-     * @param node The ement's {@link FxmlNode} counterpart. Try to avoid loading things using manual path
+     * @param node The element's {@link FxmlNode} counterpart. Try to avoid loading things using manual path
      *             as it implies losing a lot of coding safety, controller binding and stylesheet control.
      *             It does work fine as well though if you really want to, using
-     *             {@link BaseEasyFxml#loadFileAsPane(String)} after sublcassing it.
+     *             {@link BaseEasyFxml#loadNodeImpl(FXMLLoader, String)} after sublcassing it.
      *
      * @return A {@link Try} containing either the file {@link Try.Success} or the exception that was first
      * raised during the chain of nested function calls needed to load it. See {@link Try#getOrElse(Object)}
      * and related methods for how to handle {@link Try.Failure}.
      */
     Try<Pane> loadNode(final FxmlNode node);
+
+    /**
+     * Same as {@link #loadNode(FxmlNode)} but offers a selector parameter for multistage usage of
+     * {@link ControllerManager}
+     *
+     * @see ControllerManager
+     */
+    Try<Pane> loadNode(final FxmlNode node, final Object selector);
 
     /**
      * Same as {@link #loadNode(FxmlNode)} except you can choose the return type wished for instead
@@ -36,5 +47,11 @@ public interface EasyFxml {
      * @return A {@link Try} of T. Read {@link #loadNode(FxmlNode)} for more.
      */
     <T extends Node> Try<T> loadNode(final FxmlNode node, final Class<T> nodeClass);
+
+    /**
+     * This is to {@link #loadNode(FxmlNode, Class)} just what {@link #loadNode(FxmlNode, Object)} is to
+     * {@link #loadNode(FxmlNode)}.
+     */
+    <T extends Node> Try<T> loadNode(final FxmlNode node, final Class<T> nodeClass, final Object selector);
 
 }
--- src/main/java/moe/tristan/easyfxml/spring/SpringContext.java
+++ src/main/java/moe/tristan/easyfxml/spring/SpringContext.java
@@ -3,8 +3,8 @@ package moe.tristan.easyfxml.spring;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import lombok.extern.slf4j.Slf4j;
-import moe.tristan.easyfxml.FxmlController;
-import moe.tristan.easyfxml.model.beanmanagement.ControllerManager;
+import moe.tristan.easyfxml.FxmlNode;
+import moe.tristan.easyfxml.model.fxml.BaseEasyFxml;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -21,7 +21,7 @@ import static org.springframework.beans.factory.config.ConfigurableBeanFactory.S
  * bean-ify the JDK classes as can sometimes be needed.
  * <p>
  * (e.g. : here with{@link FXMLLoader} as seen in
- * {@link #fxmlLoader(ApplicationContext, ControllerManager)}}.
+ * {@link #fxmlLoader(ApplicationContext)}.
  *
  * All beans are initialized lazily so that only the used parts of the
  * library are generated. This will add a few slowdown moments in the
@@ -46,28 +46,20 @@ public class SpringContext {
      * This pre-made instance will be preloaded with
      * {@link ApplicationContext#getBean(Class)} as the default way to
      * fabricate the controller classes which handle the UI's state.
-     * The use of this is to be obtain as loose a coupling as possible
-     * between the front and back ends of the application.
-     * Once a stage is showed up it will be registered in the
-     * {@link ControllerManager} so it is reachable by other classes.
+     *
+     * See further configuration in {@link BaseEasyFxml#getSingleStageFxmlLoader(FxmlNode)}
+     * and {@link BaseEasyFxml#getMultiStageFxmlLoader(FxmlNode, Object)}
      *
      * @param context           The application's context for controller instanciation
-     * @param controllerManager The controller management service
      *
-     * @return A ready-to-use {@link FXMLLoader} which auto-registers created
-     * controllers.
+     * @return A spring-enabled {@link FXMLLoader}.
      */
     @Bean
     @Scope(scopeName = SCOPE_PROTOTYPE)
     @SuppressWarnings("unchecked")
-    public FXMLLoader fxmlLoader(final ApplicationContext context, final ControllerManager controllerManager) {
-        final FXMLLoader loader = new FXMLLoader();
-        loader.setControllerFactory(clazz -> {
-            final Class<FxmlController> clazzCtrl = (Class<FxmlController>) clazz;
-            final FxmlController controllerInstance = context.getBean(clazzCtrl);
-            controllerManager.registerSingle(clazzCtrl, controllerInstance);
-            return controllerInstance;
-        });
-        return loader;
+    public FXMLLoader fxmlLoader(final ApplicationContext context) {
+        final FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setControllerFactory(context::getBean);
+        return fxmlLoader;
     }
 }
