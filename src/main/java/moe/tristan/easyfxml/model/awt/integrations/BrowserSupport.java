77974ea10e6e3e9c42a76fa56d13103b2cada54e
Tristan Deloche <tristandeloche@gmail.com>
2017-10-05 17:50:29 +0200

    Basic architecture for AWT support checks regarding awt features

---
+++ src/main/java/moe/tristan/easyfxml/model/awt/integrations/BrowserSupport.java
@@ -0,0 +1,13 @@
+package moe.tristan.easyfxml.model.awt.integrations;
+
+import moe.tristan.easyfxml.model.awt.AwtRequired;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+
+@Component
+public class BrowserSupport implements AwtRequired {
+    public void openUrl(final URL url) {
+
+    }
+}
