a4b5e311f9244a3acfa0179c11f691301f923856
Tristan Deloche <tristandeloche@gmail.com>
2017-10-05 18:04:43 +0200

    Add URL opening through default browser support with appropriate checks.

---
+++ src/test/java/moe/tristan/easyfxml/model/awt/integrations/BrowserSupportTest.java
@@ -0,0 +1,29 @@
+package moe.tristan.easyfxml.model.awt.integrations;
+
+import moe.tristan.easyfxml.model.awt.AWTSupport;
+import moe.tristan.easyfxml.spring.SpringContext;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(classes = SpringContext.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BrowserSupportTest {
+
+    @Autowired
+    private BrowserSupport browserSupport;
+
+    @BeforeClass
+    public static void setUpAWT() {
+        AWTSupport.enableAwt();
+    }
+
+    @Test
+    public void openUrl() {
+        this.browserSupport.openUrl("https://www.google.fr");
+    }
+
+}
