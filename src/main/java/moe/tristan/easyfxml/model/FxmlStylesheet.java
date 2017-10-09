efd87c689e84eed76bd1d568159c52059fedc131
Tristan971 <tristandeloche@gmail.com>
2017-10-09 12:24:01 +0200

    Add some basic FxmlStylesheet testing

--- src/main/java/moe/tristan/easyfxml/model/FxmlStylesheet.java
+++ src/main/java/moe/tristan/easyfxml/model/FxmlStylesheet.java
@@ -1,9 +1,27 @@
 package moe.tristan.easyfxml.model;
 
-@FunctionalInterface
+import io.vavr.control.Option;
+import io.vavr.control.Try;
+import moe.tristan.easyfxml.util.PathUtils;
+
+import java.nio.file.Files;
+
 public interface FxmlStylesheet {
     /**
      * @return the CSS content that composes the stylesheet
      */
     String getCssContent();
+
+    /**
+     * Returns a FxmlStylesheet that loads content of resource file given.
+     * @param filePath just like with {@link PathUtils#getPathForResource(String)}
+     * @return The needed stylesheet wraper for {@link FxmlNode}.
+     */
+    static Option<FxmlStylesheet> ofResourceFile(final String filePath) {
+        final Try<FxmlStylesheet> loadResult = PathUtils.getPathForResource(filePath)
+            .mapTry(Files::readAllBytes)
+            .map(String::new)
+            .map(content -> (FxmlStylesheet) () -> content);
+        return loadResult.toOption();
+    }
 }
