3ebde628608b9880ec11315a45eba5295e460f3d
Tristan971 <tristandeloche@gmail.com>
2017-10-09 11:46:54 +0200

    Add basic error handling for BaseEasFxml and a few test edits

---
+++ src/test/java/moe/tristan/easyfxml/model/FxmlNodeTest.java
@@ -0,0 +1,26 @@
+package moe.tristan.easyfxml.model;
+
+import io.vavr.control.Option;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FxmlNodeTest {
+
+    final FxmlNode testNode = new FxmlNode() {
+        @Override
+        public FxmlFile getFxmlFile() {
+            return null;
+        }
+
+        @Override
+        public Option<Class<? extends FxmlController>> getControllerClass() {
+            return null;
+        }
+    };
+
+    @Test
+    public void non_overriden_css() {
+        assertThat(this.testNode.getStylesheet().isEmpty());
+    }
+}
