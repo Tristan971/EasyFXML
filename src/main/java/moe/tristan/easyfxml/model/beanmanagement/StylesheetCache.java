49d16ac83f32377b414533007e5c17e1b4685975
Tristan971 <tristandeloche@gmail.com>
2017-10-11 15:25:46 +0200

    Updates to testing FxmlStylesheet

--- src/main/java/moe/tristan/easyfxml/model/beanmanagement/StylesheetCache.java
+++ src/main/java/moe/tristan/easyfxml/model/beanmanagement/StylesheetCache.java
@@ -6,31 +6,33 @@ import moe.tristan.easyfxml.util.PathUtils;
 import org.assertj.core.util.VisibleForTesting;
 
 import java.nio.file.Files;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class StylesheetCache {
     private static final StylesheetCache INSTANCE = new StylesheetCache();
-    
+
     @VisibleForTesting
     protected final ConcurrentHashMap<FxmlStylesheet, String> cacheMap = new ConcurrentHashMap<>();
-    
+
     @VisibleForTesting
     protected StylesheetCache() {}
-    
+
     public static StylesheetCache getInstance() {
         return INSTANCE;
     }
-    
+
     public Try<String> tryLoadContent(final FxmlStylesheet stylesheet) {
         return Try.of(() -> stylesheet)
-            .map(cacheMap::get)
+            .map(this.cacheMap::get)
+            .filter(Objects::nonNull)
             .recoverWith(reason -> this.readAndCacheFileContent(stylesheet));
     }
-    
-    private Try<String> readAndCacheFileContent(FxmlStylesheet stylesheet) {
+
+    private Try<String> readAndCacheFileContent(final FxmlStylesheet stylesheet) {
         return PathUtils.getPathForResource(stylesheet.getFilePath())
             .mapTry(Files::readAllBytes)
             .map(String::new)
-            .onSuccess(content -> cacheMap.put(stylesheet, content));
+            .onSuccess(content -> this.cacheMap.put(stylesheet, content));
     }
 }
--- src/test/java/moe/tristan/easyfxml/model/beanmanagement/StylesheetCacheTest.java
+++ src/test/java/moe/tristan/easyfxml/model/beanmanagement/StylesheetCacheTest.java
@@ -1,38 +1,49 @@
 package moe.tristan.easyfxml.model.beanmanagement;
 
+import io.vavr.control.Try;
 import moe.tristan.easyfxml.model.FxmlStylesheet;
 import moe.tristan.easyfxml.spring.SpringContext;
+import moe.tristan.easyfxml.util.PathUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Optional;
 
+import static io.vavr.API.unchecked;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.CREATE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @ContextConfiguration(classes = SpringContext.class)
 @RunWith(SpringRunner.class)
 public class StylesheetCacheTest {
-    
+
     private static final class TestStylesheetCache extends StylesheetCache {
-    
+
         public TestStylesheetCache() {
             super();
         }
-        
+
         public Map<FxmlStylesheet, String> getCacheMap() {
             return this.cacheMap;
         }
     }
     private TestStylesheetCache testCache;
-    
+
     @Before
     public void setUp() {
-        testCache = new TestStylesheetCache();
+        this.testCache = new TestStylesheetCache();
     }
-    
+
     @Test
     public void getInstance() {
         assertThat(StylesheetCache.getInstance()).isNotNull();
@@ -40,10 +51,62 @@ public class StylesheetCacheTest {
         final StylesheetCache ref2 = StylesheetCache.getInstance();
         assertThat(ref1).isEqualTo(ref2);
     }
-    
+
     @Test
     public void try_load_success() {
         final FxmlStylesheet stylesheet = () -> "fxml/test_style.css";
-        testCache.tryLoadContent(() -> "fxml/test_style.css");
+        final Try<String> loadedStyle = this.testCache.tryLoadContent(stylesheet);
+        final Try<String> expectedStyle = PathUtils.getPathForResource("fxml/test_style.css")
+            .map(unchecked(Files::readAllBytes))
+            .map(String::new);
+
+        assertThat(loadedStyle.isSuccess()).isTrue();
+        assertThat(expectedStyle.isSuccess()).isTrue();
+        assertThat(loadedStyle.get()).isEqualToIgnoringWhitespace(expectedStyle.get());
+    }
+
+    @Test
+    public void reads_only_once() throws IOException {
+        final String FILE_NAME = "test_style.css";
+
+        final Try<String> cssFile = PathUtils.getPathForResource("fxml/test_style.css")
+            .map(unchecked(Files::readAllBytes))
+            .map(String::new);
+
+        assertThat(cssFile.isSuccess()).isTrue();
+
+        final Path testPath = Paths.get(System.getProperty("java.io.tmpdir")).resolve(FILE_NAME);
+        Files.deleteIfExists(testPath);
+
+        cssFile.peek(cssContent ->  {
+            try {
+                Files.write(testPath, cssContent.getBytes(UTF_8), CREATE);
+            } catch (final IOException e) {
+                fail("Couldn't write original css file", e);
+            }
+        });
+
+        final FxmlStylesheet stylesheet = () -> testPath.toAbsolutePath().toString();
+        final Try<String> stylesheetContentOriginal = stylesheet.getContentOfSheet();
+
+        cssFile.peek(cssContent -> {
+            final Optional<String> reduce = cssContent.chars()
+                .mapToObj(character -> (char) (character + 1))
+                .map(character -> "" + character)
+                .reduce((s1, s2) -> s1 + s2);
+            assertThat(reduce.isPresent()).isTrue();
+            reduce.ifPresent(shifted -> {
+                try {
+                    Files.delete(testPath);
+                    Files.write(testPath, shifted.getBytes(UTF_8), CREATE);
+                } catch (final IOException e) {
+                    fail("IO Error", e);
+                }
+            });
+        });
+
+        final Try<String> stylesheetContentCached = stylesheet.getContentOfSheet();
+
+        assertThat(stylesheetContentOriginal.get()).isEqualToIgnoringWhitespace(stylesheetContentCached.get());
     }
 }
