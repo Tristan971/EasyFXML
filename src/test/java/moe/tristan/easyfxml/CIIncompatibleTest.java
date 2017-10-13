1c823ab664de34f32c2902239d92f2cabd4b33ff
Tristan971 <tristandeloche@gmail.com>
2017-10-13 16:47:05 +0200

    Adds more checks for notray and locally headless systems

--- src/test/java/moe/tristan/easyfxml/CIIncompatibleTest.java
+++ src/test/java/moe/tristan/easyfxml/CIIncompatibleTest.java
@@ -1,8 +1,11 @@
 package moe.tristan.easyfxml;
 
-import org.junit.Before;
-
 import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
+
+import java.awt.Desktop;
+import java.awt.SystemTray;
+import org.junit.Before;
 
 /**
  * This class skips all extending test classes tests if we are in a CI
@@ -17,6 +20,9 @@ public class CIIncompatibleTest {
         final String envProperty = System.getProperty("env");
         if (envProperty != null) {
             assumeFalse("ci".equals(envProperty));
+        } else {
+            assumeTrue(SystemTray.isSupported());
+            assumeTrue(Desktop.isDesktopSupported());
         }
         System.out.println("WAS NOT CI ENVIRONMENT. EXECUTING TEST.");
     }
