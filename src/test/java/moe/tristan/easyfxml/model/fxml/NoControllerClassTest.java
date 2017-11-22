4a6ac9a37a4811cdf9284ad54fcadfc65f07b6bc
Tristan Deloche <tristan.deloche@prestataire.ing.fr>
2017-11-22 15:46:30 +0100

    Try out circle-ci cause you never know

--- src/test/java/moe/tristan/easyfxml/model/fxml/NoControllerClassTest.java
+++ src/test/java/moe/tristan/easyfxml/model/fxml/NoControllerClassTest.java
@@ -1,9 +1,15 @@
 package moe.tristan.easyfxml.model.fxml;
 
+import moe.tristan.easyfxml.spring.SpringContext;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ContextConfiguration(classes = SpringContext.class)
+@RunWith(SpringRunner.class)
 public class NoControllerClassTest {
 
     @Test
