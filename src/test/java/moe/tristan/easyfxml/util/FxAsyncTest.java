0cd3cb83948e732252d0b9cd78dde1f808133477
Tristan971 <tristandeloche@gmail.com>
2018-01-31 22:31:55 +0100

    Add rudimentary testing of FxAsync class.

---
+++ src/test/java/moe/tristan/easyfxml/util/FxAsyncTest.java
@@ -0,0 +1,45 @@
+package moe.tristan.easyfxml.util;
+
+import javafx.application.Platform;
+import moe.tristan.easyfxml.spring.SpringContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration(classes = SpringContext.class)
+@RunWith(SpringRunner.class)
+public class FxAsyncTest extends ApplicationTest {
+
+    private Thread fxThread = null;
+
+    @Before
+    public void setUp() throws ExecutionException, InterruptedException {
+        final CompletableFuture<Thread> fxThreadFetch = new CompletableFuture<>();
+        Platform.runLater(() -> fxThreadFetch.complete(Thread.currentThread()));
+        fxThread =fxThreadFetch.get();
+    }
+
+    @Test
+    public void doOnFxThread() throws ExecutionException, InterruptedException {
+        FxAsync.doOnFxThread(
+            fxThread,
+            expected -> assertThat(Thread.currentThread()).isEqualTo(expected)
+        ).toCompletableFuture().get();
+    }
+
+    @Test
+    public void computeOnFxThread() throws ExecutionException, InterruptedException {
+        FxAsync.computeOnFxThread(
+            fxThread,
+            expected -> assertThat(Thread.currentThread()).isEqualTo(expected)
+        ).toCompletableFuture().get();
+    }
+}
