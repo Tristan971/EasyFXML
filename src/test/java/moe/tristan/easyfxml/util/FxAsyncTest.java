3908ba31efdb9bf207a0e1c034ee2403b829de35
Tristan971 <tristandeloche@gmail.com>
2018-03-04 18:17:32 +0100

    A lot of improvements to the IO utility classes to get valuable stack traces instead of empty NPEs

--- src/test/java/moe/tristan/easyfxml/util/FxAsyncTest.java
+++ src/test/java/moe/tristan/easyfxml/util/FxAsyncTest.java
@@ -1,21 +1,15 @@
 package moe.tristan.easyfxml.util;
 
+import javafx.application.Platform;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.testfx.framework.junit.ApplicationTest;
-
-import javafx.application.Platform;
-import moe.tristan.easyfxml.spring.SpringContext;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ContextConfiguration(classes = SpringContext.class)
-@RunWith(SpringRunner.class)
 public class FxAsyncTest extends ApplicationTest {
 
     private Thread fxThread = null;
--- src/test/java/moe/tristan/easyfxml/util/ResourcesTest.java
+++ src/test/java/moe/tristan/easyfxml/util/ResourcesTest.java
@@ -1,12 +1,10 @@
 package moe.tristan.easyfxml.util;
 
 import io.vavr.control.Try;
-import moe.tristan.easyfxml.spring.SpringContext;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -14,17 +12,13 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ContextConfiguration(classes = SpringContext.class)
-@RunWith(SpringRunner.class)
 public class ResourcesTest {
 
-    private static final String PATH_UTIL_TESTS_FOLDER = "PathUtilsTests/";
+    private static final String PATH_UTIL_TESTS_FOLDER = "PathUtilsTests" + File.separator;
 
     private static final String EXISTING_FILE_NAME = "file_that_exists";
     private static final String EXISTING_FILE_CONTENT = "file_that_exists_content";
-
     private static final String EXISTING_ANOTHER_FILE_NAME = "another_file_that_exists";
-
     private static final String NONEXISTING_FILE_NAME = "file_that_doesnt_exist";
 
     @Test
@@ -42,13 +36,25 @@ public class ResourcesTest {
     }
 
     @Test
-    public void path_of_nonexisting_file() {
+    public void getResourcePath_should_display_path_tried() {
         final Try<Path> fileThatDoesntExist = Resources.getResourcePath(
             PATH_UTIL_TESTS_FOLDER + NONEXISTING_FILE_NAME
         );
 
         assertThat(fileThatDoesntExist.isFailure()).isTrue();
         assertThat(fileThatDoesntExist.getCause().getMessage()).contains(PATH_UTIL_TESTS_FOLDER+NONEXISTING_FILE_NAME);
+        fileThatDoesntExist.getCause().printStackTrace();
+    }
+
+    @Test
+    public void getResourceURL_should_display_path_tried() {
+        final Try<URL> fileThatDoesntExist = Resources.getResourceURL(
+            PATH_UTIL_TESTS_FOLDER + NONEXISTING_FILE_NAME
+        );
+
+        assertThat(fileThatDoesntExist.isFailure()).isTrue();
+        assertThat(fileThatDoesntExist.getCause().getMessage()).contains(PATH_UTIL_TESTS_FOLDER+NONEXISTING_FILE_NAME);
+        fileThatDoesntExist.getCause().printStackTrace();
     }
 
     @Test
