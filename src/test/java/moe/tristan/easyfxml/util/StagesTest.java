a0f1aeb75594e7fa7f12137295d5b38c33da88a4
Tristan Deloche <tristandeloche@gmail.com>
2018-03-05 02:11:13 +0100

    Add some light testing to Stages#setStylesheet

--- src/test/java/moe/tristan/easyfxml/util/StagesTest.java
+++ src/test/java/moe/tristan/easyfxml/util/StagesTest.java
@@ -1,68 +1,87 @@
 package moe.tristan.easyfxml.util;
 
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import moe.tristan.easyfxml.spring.SpringContext;
+import moe.tristan.easyfxml.api.FxmlStylesheet;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.testfx.framework.junit.ApplicationTest;
 
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ContextConfiguration(classes = SpringContext.class)
-@RunWith(SpringRunner.class)
 public class StagesTest extends ApplicationTest {
 
-    private static String STAGE_TITLE;
-    private static String STAGE_TITLE_2;
-    private static Pane STAGE_PANE;
-    private static Stage TEST_STAGE;
+    private static final FxmlStylesheet TEST_STYLE = new FxmlStylesheet() {
+        @Override
+        public String getExternalForm() {
+            return Resources.getResourceURL("fxml/test_style.css")
+                            .getOrElseThrow((Function<? super Throwable, RuntimeException>) RuntimeException::new)
+                            .toExternalForm();
+        }
+    };
+
+    private Stage testStage;
+    private String stageTitle;
+    private String stageTitle2;
+    private Pane stagePane;
 
     @Override
     public void start(final Stage stage) {
         Platform.setImplicitExit(false);
-        STAGE_TITLE = "STAGE_TITLE";
-        STAGE_TITLE_2 = "STAGE_TITLE_2";
-        STAGE_PANE = new Pane();
-        TEST_STAGE = stage;
+        stageTitle = "stageTitle";
+        stageTitle2 = "stageTitle2";
+        stagePane = new Pane();
+        testStage = stage;
+        testStage.setScene(new Scene(stagePane));
         stage.show();
         stage.hide();
     }
 
     @Test
     public void stageOf() throws ExecutionException, InterruptedException {
-        Stages.stageOf(STAGE_TITLE, STAGE_PANE)
+        final Pane newPane = new Pane();
+        Stages.stageOf(stageTitle, newPane)
               .thenAccept(stage -> {
-                  assertThat(stage.getScene().getRoot()).isEqualTo(STAGE_PANE);
-                  assertThat(stage.getTitle()).isEqualTo(STAGE_TITLE);
+                  assertThat(stage.getScene().getRoot()).isEqualTo(newPane);
+                  assertThat(stage.getTitle()).isEqualTo(stageTitle);
               })
               .toCompletableFuture().get();
     }
 
     @Test
     public void scheduleDisplaying() throws ExecutionException, InterruptedException {
-        Stages.scheduleDisplaying(TEST_STAGE)
-              .thenAccept(stage -> assertThat(TEST_STAGE.isShowing()).isTrue())
+        Stages.scheduleDisplaying(testStage)
+              .thenAccept(stage -> assertThat(testStage.isShowing()).isTrue())
               .toCompletableFuture().get();
     }
 
     @Test
     public void scheduleHiding() throws ExecutionException, InterruptedException {
-        Stages.scheduleHiding(TEST_STAGE)
-              .thenAccept(stage -> assertThat(TEST_STAGE.isShowing()).isFalse())
+        Stages.scheduleHiding(testStage)
+              .thenAccept(stage -> assertThat(testStage.isShowing()).isFalse())
               .toCompletableFuture().get();
     }
 
     @Test
     public void asyncStageOperation() throws ExecutionException, InterruptedException {
-        FxAsync.doOnFxThread(TEST_STAGE, stage -> stage.setTitle(STAGE_TITLE_2))
-               .thenAccept(stage -> assertThat(stage.getTitle()).isEqualTo(STAGE_TITLE_2))
+        FxAsync.doOnFxThread(testStage, stage -> stage.setTitle(stageTitle2))
+               .thenAccept(stage -> assertThat(stage.getTitle()).isEqualTo(stageTitle2))
                .toCompletableFuture().get();
+    }
+
+    @Test
+    public void setStylesheet() throws ExecutionException, InterruptedException {
+        final CompletionStage<Stage> setStyleAsyncOp = Stages.setStylesheet(testStage, TEST_STYLE);
+        final Stage stage = setStyleAsyncOp.toCompletableFuture().get();
+        final ObservableList<String> stylesheets = stage.getScene().getStylesheets();
+        assertThat(stylesheets).hasSize(1);
+        assertThat(stylesheets).containsExactly(TEST_STYLE.getExternalForm());
     }
 }
