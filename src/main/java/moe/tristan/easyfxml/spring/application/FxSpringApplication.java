bd65ff19b8174d1b2f303d8c476f350bf3430e8b
Tristan971 <tristandeloche@gmail.com>
2018-03-13 01:47:08 +0100

    Add better SpringApplicationBuilder configuration and possible override

--- src/main/java/moe/tristan/easyfxml/spring/application/FxSpringApplication.java
+++ src/main/java/moe/tristan/easyfxml/spring/application/FxSpringApplication.java
@@ -1,7 +1,8 @@
 package moe.tristan.easyfxml.spring.application;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import javafx.application.Application;
@@ -14,7 +15,17 @@ public abstract class FxSpringApplication extends Application {
 
     @Override
     public void init() {
-        this.context = SpringApplication.run(getClass());
+        this.context = getSab().run();
+    }
+
+    protected SpringApplicationBuilder getSab() {
+        return new SpringApplicationBuilder(getClass())
+            .headless(false)
+            .web(WebApplicationType.NONE);
+    }
+
+    protected void beforeSpringInit() {
+        // do nothing by default
     }
 
     @Override
