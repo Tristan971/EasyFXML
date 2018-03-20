e81c56bc41874eabd3756925d2c867d6d5b5fcf9
Tristan971 <tristandeloche@gmail.com>
2018-03-20 18:48:04 +0100

    Improve Browser opening tests

--- src/main/java/moe/tristan/easyfxml/model/awt/integrations/BrowserSupport.java
+++ src/main/java/moe/tristan/easyfxml/model/awt/integrations/BrowserSupport.java
@@ -1,18 +1,13 @@
 package moe.tristan.easyfxml.model.awt.integrations;
 
 import org.springframework.stereotype.Component;
-import moe.tristan.easyfxml.model.awt.AwtUtils;
 import moe.tristan.easyfxml.model.exception.ExceptionHandler;
-import io.vavr.CheckedFunction1;
 import io.vavr.control.Try;
 
 import java.awt.Desktop;
 import java.net.URI;
 import java.net.URL;
 import java.util.Objects;
-import java.util.concurrent.CompletionStage;
-
-import static io.vavr.API.unchecked;
 
 /**
  * This class contains some utility methods to open URLs with the default web browser of the user.
@@ -25,11 +20,11 @@ public class BrowserSupport {
      *
      * @param url The URL as a String. Must conform to {@link URL#URL(String)}.
      */
-    public void openUrl(final String url) {
-        Try.of(() -> url)
-           .map(unchecked((CheckedFunction1<String, URL>) URL::new))
-           .onSuccess(this::openUrl)
-           .onFailure(cause -> this.onException(cause, url));
+    public Try<Void> openUrl(final String url) {
+        return Try.of(() -> url)
+                  .mapTry(URL::new)
+                  .flatMap(this::openUrl)
+                  .onFailure(cause -> this.onException(cause, url));
     }
 
     /**
@@ -37,11 +32,16 @@ public class BrowserSupport {
      *
      * @param url The URL as an {@link URL}.
      */
-    public void openUrl(final URL url) {
-        Try.of(() -> url)
-           .map(unchecked(URL::toURI))
-           .onSuccess(this::browse)
-           .onFailure(cause -> this.onException(cause, Objects.toString(url)));
+    public Try<Void> openUrl(final URL url) {
+        return Try.of(() -> url)
+                  .mapTry(URL::toURI)
+                  .flatMap(this::browse)
+                  .onFailure(cause -> this.onException(cause, Objects.toString(url)));
+    }
+
+    private Try<Void> browse(final URI uri) {
+        return Try.run(() -> Desktop.getDesktop().browse(uri))
+                  .onFailure(cause -> onException(cause, uri.toString()));
     }
 
     private void onException(final Throwable cause, final String url) {
@@ -49,17 +49,6 @@ public class BrowserSupport {
             "Browser error",
             "We could not open the following url in the browser : " + url,
             cause
-        );
-    }
-
-    private void browse(final URI uri) {
-        final CompletionStage<Try<Void>> browserOpeningResult = AwtUtils.asyncAwtCallbackWithRequirement(
-            Desktop::getDesktop,
-            desktop -> Try.run(() -> desktop.browse(uri))
-        );
-
-        browserOpeningResult.thenAccept(
-            result -> result.onFailure(cause -> onException(cause, uri.toString()))
         );
     }
 }
--- src/test/java/moe/tristan/easyfxml/model/awt/integrations/BrowserSupportTest.java
+++ src/test/java/moe/tristan/easyfxml/model/awt/integrations/BrowserSupportTest.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import moe.tristan.easyfxml.spring.application.FxSpringContext;
+import io.vavr.control.Try;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.testfx.framework.junit.ApplicationTest;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ContextConfiguration(classes = FxSpringContext.class)
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,18 +22,29 @@ public class BrowserSupportTest extends ApplicationTest {
     private BrowserSupport browserSupport;
 
     @Test
-    public void openUrl_good_url() {
-        browserSupport.openUrl("https://www.google.fr");
+    public void openUrl_good_url() throws Throwable {
+        Try<Void> validUrlOpen = browserSupport.openUrl("https://www.google.fr");
+
+        if (validUrlOpen.isFailure()) {
+            throw validUrlOpen.getCause();
+        }
+        assertThat(validUrlOpen.isSuccess()).isTrue();
     }
 
     @Test
     public void openUrl_bad_url() {
-        browserSupport.openUrl("not_a_url");
+        Try<Void> invalidUrlOpening = browserSupport.openUrl("not_a_url");
+
+        assertThat(invalidUrlOpening.isFailure()).isTrue();
+        assertThat(invalidUrlOpening.getCause()).isInstanceOf(MalformedURLException.class);
     }
 
     @Test
     public void browse_ioe() {
-        browserSupport.openUrl((URL) null);
+        Try<Void> nullUrlOpening = browserSupport.openUrl((URL) null);
+
+        assertThat(nullUrlOpening.isFailure()).isTrue();
+        assertThat(nullUrlOpening.getCause()).isInstanceOf(NullPointerException.class);
     }
 
 }
