7402449bbad3566bbc16f9b97b66ce4c1b86df03
Tristan Deloche <tristandeloche@gmail.com>
2018-03-23 15:07:26 +0100

    Clean up some Javadoc

--- src/main/java/moe/tristan/easyfxml/model/awt/integrations/BrowserSupport.java
+++ src/main/java/moe/tristan/easyfxml/model/awt/integrations/BrowserSupport.java
@@ -8,6 +8,7 @@ import java.awt.Desktop;
 import java.net.URI;
 import java.net.URL;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * This class contains some utility methods to open URLs with the default web browser of the user.
@@ -19,6 +20,10 @@ public class BrowserSupport {
      * Opens a given URL or a pop-up with the error if it could not do so.
      *
      * @param url The URL as a String. Must conform to {@link URL#URL(String)}.
+     *
+     * @return a {@link Try} that can be either {@link Try.Success} (and empty) or {@link Try.Failure} and contain an
+     * exception.
+     * @see Try#onFailure(Consumer)
      */
     public Try<Void> openUrl(final String url) {
         return Try.of(() -> url)
@@ -31,6 +36,10 @@ public class BrowserSupport {
      * Opens a given URL or a pop-up with the error if it could not do so.
      *
      * @param url The URL as an {@link URL}.
+     *
+     * @return a {@link Try} that can be either {@link Try.Success} (and empty) or {@link Try.Failure} and contain an
+     * exception.
+     * @see Try#onFailure(Consumer)
      */
     public Try<Void> openUrl(final URL url) {
         return Try.of(() -> url)
