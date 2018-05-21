d74aaa4422b627a9f3358f62b931279dbfb8d9f7
Tristan971 <tristandeloche@gmail.com>
2018-05-21 21:08:18 +0200

    Document new classes.

--- src/main/java/moe/tristan/easyfxml/model/beanmanagement/Selector.java
+++ src/main/java/moe/tristan/easyfxml/model/beanmanagement/Selector.java
@@ -1,21 +1,47 @@
 package moe.tristan.easyfxml.model.beanmanagement;
 
+import moe.tristan.easyfxml.EasyFxml;
+
 import java.lang.ref.WeakReference;
 import java.util.Objects;
 
+/**
+ * Selectors are special wrappers around a value used in an {@link AbstractInstanceManager} to distinguish
+ * multiple children of the mapped class amongst themselves.
+ * It uses a weak reference as a way to store the actual selector value.
+ * The overriding of equals and hashCode makes it transparent to use.
+ *
+ * Its main use is to avoid confusion in the multiple signatures of {@link EasyFxml}.
+ *
+ * @see AbstractInstanceManager
+ * @see EasyFxml
+ */
 public class Selector {
 
     private final WeakReference<?> reference;
 
+    /**
+     * Creates a Selector from a given object.
+     * @param reference The actual value of the selector to use in map-based storage implementations.
+     */
     public Selector(Object reference) {
         this.reference = new WeakReference<>(reference);
     }
 
+    /**
+     * @return the backing reference.
+     */
     @SuppressWarnings("WeakerAccess")
     public WeakReference<?> getReference() {
         return reference;
     }
 
+    /**
+     * Two Selectors are equals iff the values backing their weak references are the same.
+     * Null is supported.
+     * @param o The other selector to check for equality with.
+     * @return true if this selector and the one passed as parameter have the same backing value.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -27,6 +53,9 @@ public class Selector {
         return Objects.equals(thisRef, oRef);
     }
 
+    /**
+     * @return The hashcode of the backing value. 0 if it is null. See {@link Objects#hashCode(Object)}.
+     */
     @Override
     public int hashCode() {
         final Object thisRef = reference.get();
--- src/main/java/moe/tristan/easyfxml/model/fxml/FxmlLoadResult.java
+++ src/main/java/moe/tristan/easyfxml/model/fxml/FxmlLoadResult.java
@@ -1,21 +1,63 @@
 package moe.tristan.easyfxml.model.fxml;
 
+import io.vavr.Tuple;
+import io.vavr.collection.List;
+import io.vavr.collection.Seq;
 import io.vavr.control.Try;
 
-public class FxmlLoadResult<NODE_TYPE, CONTROLLER_TYPE> {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+/**
+ * This class wraps a node load result as a tuple of {@link Try} instances. Respectively one for the node itself (loaded
+ * through {@link FXMLLoader}) and one for the controller associated with this node (loaded through Spring's context
+ * using {@link org.springframework.context.ApplicationContext#getBean(String)}).
+ *
+ * @param <NODE_TYPE>       The type of the node loaded
+ * @param <CONTROLLER_TYPE> The type of the controller bound to it
+ */
+public class FxmlLoadResult<NODE_TYPE, CONTROLLER_TYPE> implements Tuple {
     private final Try<NODE_TYPE> node;
     private final Try<CONTROLLER_TYPE> controller;
 
+    /**
+     * Creates a {@link FxmlLoadResult} from two {@link Try} values.
+     *
+     * @param node       The node load result
+     * @param controller The controller load result
+     */
     public FxmlLoadResult(Try<NODE_TYPE> node, Try<CONTROLLER_TYPE> controller) {
         this.node = node;
         this.controller = controller;
     }
 
+    /**
+     * @return The result of the attempt to load the {@link Node} as a {@link Try} instance.
+     */
     public Try<NODE_TYPE> getNode() {
         return node;
     }
 
+    /**
+     * @return The result of the attempt to build the controller associated to the node as a {@link Try} instance.
+     */
     public Try<CONTROLLER_TYPE> getController() {
         return controller;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int arity() {
+        return 2;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Seq<?> toSeq() {
+        return List.of(node, controller);
     }
 }
