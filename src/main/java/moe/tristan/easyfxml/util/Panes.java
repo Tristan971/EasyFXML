44ee10c603cb8b45bf26ee196dbd4bcbbb915a5b
Tristan Deloche <tristandeloche@gmail.com>
2018-05-25 01:52:38 +0200

    Improve variable naming.

--- src/main/java/moe/tristan/easyfxml/util/Panes.java
+++ src/main/java/moe/tristan/easyfxml/util/Panes.java
@@ -17,16 +17,16 @@ public final class Panes {
      * Sets as the sole content of a pane another Node. This is supposed to work as having a first Pane being the wanted
      * display zone and the second Node the displayed content.
      *
-     * @param container The container defining the displayable zone, as a {@link Pane}.
+     * @param parent The container defining the displayable zone, as a {@link Pane}.
      * @param content   The content to display
      * @param <T>       The subtype if necessary of the container
      *
      * @return A {@link CompletionStage} to have monitoring over the state of the asynchronous operation
      */
-    public static <T extends Pane> CompletionStage<T> setContent(final T container, final Node content) {
-        return FxAsync.doOnFxThread(container, _container -> {
-            _container.getChildren().clear();
-            _container.getChildren().add(content);
+    public static <T extends Pane> CompletionStage<T> setContent(final T parent, final Node content) {
+        return FxAsync.doOnFxThread(parent, parentNode -> {
+            parentNode.getChildren().clear();
+            parentNode.getChildren().add(content);
         });
     }
 }
