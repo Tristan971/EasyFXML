70d1e67a4b04a2b751e638ee2c65d1482e8a7d97
Tristan Deloche <tristandeloche@gmail.com>
2018-06-24 03:07:37 +0200

    Better superclass choice to allow #not #and and other nice bindings

--- src/main/java/moe/tristan/easyfxml/model/components/listview/ComponentCellFxmlController.java
+++ src/main/java/moe/tristan/easyfxml/model/components/listview/ComponentCellFxmlController.java
@@ -2,11 +2,11 @@ package moe.tristan.easyfxml.model.components.listview;
 
 import moe.tristan.easyfxml.api.FxmlController;
 
-import javafx.beans.value.ObservableValue;
+import javafx.beans.binding.BooleanExpression;
 
 public interface ComponentCellFxmlController<T> extends FxmlController {
 
     void updateWithValue(final T newValue);
-    <U extends ObservableValue<Boolean>> void selectedProperty(final U selected);
+    void selectedProperty(final BooleanExpression selected);
 
 }
