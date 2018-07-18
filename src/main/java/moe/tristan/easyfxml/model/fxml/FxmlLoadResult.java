cce112fdd9b11bc2714d616d7b5f60d4fcbd10e9
Tristan971 <tristandeloche@gmail.com>
2018-07-18 18:21:17 +0200

    Add some convenience post-loading methods on FxmlLoadResult class

--- src/main/java/moe/tristan/easyfxml/model/fxml/FxmlLoadResult.java
+++ src/main/java/moe/tristan/easyfxml/model/fxml/FxmlLoadResult.java
@@ -6,10 +6,15 @@ import io.vavr.Tuple;
 import io.vavr.collection.List;
 import io.vavr.collection.Seq;
 import io.vavr.control.Try;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
+
+import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * This class wraps a node load result as a tuple of {@link Try} instances. Respectively one for the node itself (loaded
@@ -20,6 +25,9 @@ import javafx.scene.layout.Pane;
  * @param <CONTROLLER> The type of the controller bound to it
  */
 public class FxmlLoadResult<NODE extends Node, CONTROLLER extends FxmlController> implements Tuple {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FxmlLoadResult.class);
+
     private final Try<NODE> node;
     private final Try<CONTROLLER> controller;
 
@@ -32,6 +40,42 @@ public class FxmlLoadResult<NODE extends Node, CONTROLLER extends FxmlController
     public FxmlLoadResult(Try<NODE> node, Try<CONTROLLER> controller) {
         this.node = node;
         this.controller = controller;
+    }
+
+    public FxmlLoadResult<NODE, CONTROLLER> afterNodeLoaded(final Consumer<Node> onNodeLoaded) {
+        ensureCorrectlyLoaded();
+        return new FxmlLoadResult<>(
+                node.map(val -> {
+                    onNodeLoaded.accept(val);
+                    return val;
+                }),
+                controller
+        );
+    }
+
+    public FxmlLoadResult<NODE, CONTROLLER> afterControllerLoaded(final Consumer<CONTROLLER> onControllerLoaded) {
+        ensureCorrectlyLoaded();
+        return new FxmlLoadResult<>(
+                node,
+                controller.map(val -> {
+                    onControllerLoaded.accept(val);
+                    return val;
+                })
+        );
+    }
+
+    private void ensureCorrectlyLoaded() {
+        if (node.isFailure()) {
+            LOG.error("Node did not properly load!", node.getCause());
+            throw new IllegalStateException(node.getCause());
+        }
+        if (controller.isFailure()) {
+            LOG.error("Controller did not properly load!", controller.getCause());
+            throw new IllegalStateException(controller.getCause());
+        }
+
+        Objects.requireNonNull(node.get(), "The node did not load properly and was null.");
+        Objects.requireNonNull(controller.get(), "The controller did not load properly and was null.");
     }
 
     /**
