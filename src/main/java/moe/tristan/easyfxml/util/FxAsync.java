19954444b3c36d5872f0b5c8bfc3e8942b661b9d
Tristan971 <tristandeloche@gmail.com>
2018-12-03 16:44:43 +0000

    Fix illegal javadoc tags

--- src/main/java/moe/tristan/easyfxml/util/FxAsync.java
+++ src/main/java/moe/tristan/easyfxml/util/FxAsync.java
@@ -14,8 +14,10 @@ import io.vavr.Tuple2;
  * <p>
  * Basically a glorified replacement of {@link Platform#runLater(Runnable)} that offers task-style methods and allows
  * for computational calls.
- *
- * @apiNote Be careful, the {@link CompletionStage} returned will be bounded to the JavaFX thread by default.
+ * <p>
+ * /!\ WARNING /!\
+ * <p>
+ * Be careful, the {@link CompletionStage} returned will be bounded to the JavaFX thread by default.
  * Please explicitly call {@code async} named methods on it afterwards to go back to the default async threadpool for subsequent executions.
  */
 public final class FxAsync {
@@ -31,8 +33,8 @@ public final class FxAsync {
      * @param <T>     The type of the element consumed
      *
      * @return A {@link CompletionStage} to have monitoring over the state of the asynchronous operation.
-     *
-     * @apiNote Please make sure to read this class' documentation regarding threading and these methods.
+     * <p>
+     * Please make sure to read this class' documentation regarding threading and these methods.
      */
     public static <T> CompletionStage<T> doOnFxThread(final T element, final Consumer<T> action) {
         return CompletableFuture.supplyAsync(() -> {
@@ -50,8 +52,8 @@ public final class FxAsync {
      * @param <U>     The output type
      *
      * @return A {@link CompletionStage} to have monitoring over the state of the asynchronous computation.
-     *
-     * @apiNote Please make sure to read this class' documentation regarding threading and these methods.
+     * <p>
+     * Please make sure to read this class' documentation regarding threading and these methods.
      */
     public static <T, U> CompletionStage<U> computeOnFxThread(final T element, final Function<T, U> compute) {
         return CompletableFuture.supplyAsync(() -> compute.apply(element), Platform::runLater);
