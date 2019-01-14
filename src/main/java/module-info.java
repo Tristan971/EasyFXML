712d3cc9c9e7c47e26ed92c2fe047381a6c64110
Tristan Deloche <tristandeloche@gmail.com>
2019-01-14 16:29:19 +0100

    Finally properly enable JPMS usage

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+open module easyfxml {
+    requires java.annotation;
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.core;
+
+    requires slf4j.api;
+
+    requires io.vavr;
+    requires awaitility;
+}
