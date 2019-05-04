6e9a7d34f94023557673b965432476aaf6601caa
Tristan971 <tristandeloche@gmail.com>
2019-05-04 20:14:23 +0100

    Start fleshing out platform setup a bit

---
+++ easyfxml-native/easyfxml-platform/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+open module moe.tristan.easyfxml.natives.platform {
+
+    exports moe.tristan.easyfxml.natives.platform;
+
+    requires java.annotation;
+
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.core;
+
+    requires oshi.core;
+    requires slf4j.api;
+
+}
