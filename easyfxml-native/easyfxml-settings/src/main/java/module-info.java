ff26c74141b99cd3829191934a57ccfd9f965d55
Tristan971 <tristandeloche@gmail.com>
2019-05-04 20:19:26 +0100

    Make settings management depend on platform detection

---
+++ easyfxml-native/easyfxml-settings/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module easyfxml.settings {
+
+    exports moe.tristan.easyfxml.natives.settings;
+    exports moe.tristan.easyfxml.natives.settings.model;
+
+    requires spring.beans;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+
+}
