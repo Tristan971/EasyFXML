2d11f6e8a48ff981116852f827e4331c476f663f
Tristan Deloche <tristan.deloche@treatwell.com>
2019-05-17 17:21:07 +0100

    Start adding a couple more helper classes for properties.

--- easyfxml/src/main/java/moe/tristan/easyfxml/util/Properties.java
+++ easyfxml/src/main/java/moe/tristan/easyfxml/util/Properties.java
@@ -17,26 +17,37 @@
 package moe.tristan.easyfxml.util;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.Property;
+import javafx.beans.value.ObservableValue;
 
 public final class Properties {
 
     private Properties() {
     }
 
-    public static <T, P extends Property<T>> P newPropertyWithCallback(Supplier<P> propertyFactory, Consumer<T> callback) {
+    public static <T, P extends ObservableValue<T>> P newPropertyWithCallback(Supplier<P> propertyFactory, Consumer<T> callback) {
         final P property = propertyFactory.get();
         whenPropertyIsSet(property, callback);
         return property;
     }
 
-    public static <T, P extends Property<T>> void whenPropertyIsSet(P property, Consumer<T> doWhenSet) {
+    public static <T, P extends ObservableValue<T>> void whenPropertyIsSet(P property, Consumer<T> doWhenSet) {
         whenPropertyIsSet(property, () -> doWhenSet.accept(property.getValue()));
     }
 
-    public static <T, P extends Property<T>> void whenPropertyIsSet(P property, Runnable doWhenSet) {
+    public static <A, B, P extends ObservableValue<A>> void whenPropertyIsSet(P property, Function<A, B> adapter, Consumer<B> doWhenSet) {
+        whenPropertyIsSet(property, () -> doWhenSet.accept(adapter.apply(property.getValue())));
+    }
+
+    public static <S, C> void bind(ObservableValue<S> from, Function<S, C> adapter, Property<C> to) {
+        whenPropertyIsSet(from, adapter, to::setValue);
+    }
+
+    public static <T, P extends ObservableValue<T>> void whenPropertyIsSet(P property, Runnable doWhenSet) {
         property.addListener((o, prev, cur) -> doWhenSet.run());
         if (property.getValue() != null) {
             doWhenSet.run();
