90b21f1138dc07da2fe88837e4963309d1fbe081
Tristan971 <tristandeloche@gmail.com>
2019-05-18 18:33:47 +0100

    Add basic wiring test for the user form sample

--- easyfxml-junit/src/main/java/module-info.java
+++ easyfxml-junit/src/main/java/module-info.java
@@ -4,8 +4,8 @@ open module moe.tristan.easyfxml.junit {
 
     requires transitive javafx.graphics;
 
-    requires awaitility;
-    requires org.testfx.junit;
-    requires org.testfx;
+    requires transitive awaitility;
+    requires transitive org.testfx.junit;
+    requires transitive org.testfx;
 
 }
