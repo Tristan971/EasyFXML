f8c1f47ba847eadf84da6fdf2060f77a6ba030c0
Tristan971 <tristandeloche@gmail.com>
2019-05-18 23:57:30 +0100

    Generalize recursive regex copy script

--- .circleci/copy_recursive_regex.sh
+++ .circleci/copy_recursive_regex.sh
@@ -6,9 +6,9 @@ DEST_FOLDER=$2
 rm -rf ${DEST_FOLDER}
 mkdir -p ${DEST_FOLDER}
 
-for testXml in $(find . -type f -regex ${REGEX}); do
-    echo ${testXml}
-    cp ${testXml} ${DEST_FOLDER}
+for matching_file in $(find . -type f -regex ${REGEX}); do
+    echo ${matching_file}
+    cp ${matching_file} ${DEST_FOLDER}
 done
 
 echo
