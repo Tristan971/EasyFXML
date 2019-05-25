b82d7e750960a37b77c1932b28613948efb8583c
Tristan Deloche <tristandeloche@gmail.com>
2019-05-25 22:59:30 +0100

    Sanitize recursive regex copy script

--- .circleci/copy_recursive_regex.sh
+++ .circleci/copy_recursive_regex.sh
@@ -6,9 +6,8 @@ DEST_FOLDER=$2
 rm -rf ${DEST_FOLDER}
 mkdir -p ${DEST_FOLDER}
 
-for matching_file in $(find . -type f -regex ${REGEX}); do
-    echo ${matching_file}
-    cp ${matching_file} ${DEST_FOLDER}
-done
+find . -type f -regex ${REGEX} \
+-exec echo {} \; \
+-exec cp {} ${DEST_FOLDER} \;
 
 echo
